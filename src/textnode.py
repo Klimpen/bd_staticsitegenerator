80982b0714647e1cadf8a4dbe187c0442097df97
Klimpen <axco2004@gmail.com>
2025-01-17 19:05:39 +1300

    added .gitignore for generated files, added support testing stuff, initial source files

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT_BOLD = "bold"
+    TEXT_ITALIC = "italic"
+    TEXT_CODE = "code"
+    TEXT_LINK = "link"
+    TEXT_IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, input_node):
+        return (
+            self.text == input_node.text and
+            self.text_type == input_node.text_type and
+            self.url == input_node.url)
+            
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
